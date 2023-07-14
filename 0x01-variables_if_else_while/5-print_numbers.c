0987b02ff849e35c091f48c6bf6fb068f87be0c2
ebubedike76 <ogbonnayaisaac76@gmail.com>
2023-07-14 10:49:56 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program that prints all single digit numbers from zero to ten.
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		printf("%d", x);
+	printf("\n");
+	return (0);
+}
